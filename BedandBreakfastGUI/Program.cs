372f047d212da26cf87efafe061ff70193107de2
Tcrisw094 <TCRISW9754@my.sullivan.edu>
2025-11-06 16:16:22 +0000

    Add BedandBreakfastGUI console app demonstrating System.Console and Console.WriteLine

---
+++ BedandBreakfastGUI/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace BedandBreakfastGUI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Simple console demo using System.Console and Console.WriteLine
+            Console.WriteLine("Welcome to the Bed and Breakfast GUI (console demo).");
+            Console.WriteLine("This program demonstrates using System.Console and Console.WriteLine().");
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
